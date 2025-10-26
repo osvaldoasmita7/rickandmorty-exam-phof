e89000ac8e3ea5d0cec49ab037cc59c7084987b0
Osvaldo Francisco Ponce Hernández <osvaldoasmita2019@outlook.com>
2025-10-26 14:36:07 -0600

    🐛 bugfix: initial home and fix heigth element

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/", // La ruta que queremos redirigir
+        destination: "/exam", // La nueva ruta por defecto
+        permanent: true, // Redirección permanente
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
