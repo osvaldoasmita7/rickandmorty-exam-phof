74859e98cfc203ab609f165ec9b9871bfe56aaa3
Osvaldo Francisco Ponce Hernández <osvaldoasmita2019@outlook.com>
2025-10-26 13:03:31 -0600

    ✨ feat: add jest to test

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+// jest.config.js
+
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+// Agrega cualquier configuración personalizada que se pase a Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  
+  // Patrones para recolectar la cobertura de las carpetas especificadas
+  collectCoverageFrom: [
+    // Asegura la detección de archivos en la raíz del proyecto
+    '<rootDir>/**/*.{js,jsx,ts,tsx}',
+    
+    // EXCLUSIONES ESPECÍFICAS
+    // Excluye las carpetas que no son de la aplicación
+    '!**/node_modules/**',
+    '!**/.next/**',
+    '!**/coverage/**',
+    
+    // Excluye los archivos de configuración
+    '!**/jest.config.js',
+    '!**/_tests_/**',
+    
+    // Excluye explícitamente los archivos de Next.js que no son componentes de la app
+    '!**/layout.tsx',
+    '!**/page.tsx',
+    '!**/app/interfaces/**',
+    '!**/app/types/**',
+  ],
+};
+
+module.exports = createJestConfig(customJestConfig);
